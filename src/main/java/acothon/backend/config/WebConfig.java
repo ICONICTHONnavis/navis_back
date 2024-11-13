0a84d674b1b515ec958ee7e818d7d68fbdaddc37
ansungwoo <asw4ever@naver.com>
2024-11-13 22:49:39 +0900

    fix: WebConfig 수정

--- src/main/java/acothon/backend/config/WebConfig.java
+++ src/main/java/acothon/backend/config/WebConfig.java
@@ -1,16 +1,12 @@
 package acothon.backend.config;
 
 
-import ai.platform.proma.intercepter.LoginUserArgumentResolver;
-import ai.platform.proma.intercepter.LoginUserIntercpter;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 
 @Configuration
